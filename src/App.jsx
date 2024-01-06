8a3ffe9b5f46305c603758e659c1e23ed1a95b05
can101 <can896373@gmail.com>
2024-01-06 23:48:29 +0300

    feat: integrated r3f

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
+import Experince from "./components/Experince";
+import "./App.css";
+import Interface from "./components/Interface";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ /* border: "5px solid pink",*/ height: "100vh" }}>
+        <Canvas
+          camera={{ position: [1, 1.5, 2.5], fov: 50 }}
+          style={{
+            // border: "1px solid red",
+            width: "100%",
+            height: "100%",
+            // background: "gray",
+          }}
+          shadows
+        >
+          <Experince />
+        </Canvas>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Interface />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
