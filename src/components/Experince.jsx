5ffe723d479a6de739f7dead8befa9e48460238b
can101 <can896373@gmail.com>
2024-01-06 23:47:56 +0300

    feat: integrated model

---
+++ src/components/Experince.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/no-unknown-property */
+import { OrbitControls } from "@react-three/drei";
+import Woman from "./Woman";
+
+const Experince = () => {
+  return (
+    <>
+      <OrbitControls  />
+      <ambientLight />
+      <directionalLight
+        position={[-5, 5, 5]}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      <group position={[0, -1, 0]}>
+        <Woman />
+      </group>
+      <mesh
+        rotation={[-0.5 * Math.PI, 0, 0]}
+        position={[0, -1, 0]}
+        receiveShadow
+      >
+        <planeGeometry args={[10, 10, 1, 1]} />
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
+    </>
+  );
+};
+
+export default Experince;
